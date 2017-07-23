6e9a2a9960ebc28bb7a7052f6dba44b352b7f737
Leonardo Souza <leonardodisouza@gmail.com>
2017-07-23 17:22:06 -0300

    adicionando recurso estatico e formatacao

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ import express from 'express'
 const PORT = 3000
 
 const app = express()
+app.set('json spaces', 2)
 
 app.get('/', (req, res) => res.json({ status: 'NTask API' }))
+
+app.get('/tasks', (req, res) => {
+  res.json([
+    { title: 'Fazer compras' },
+    { title: 'Consertar PC' }
+  ])
+})
 
 app.listen(PORT, console.log(`NTask API - Running on port ${PORT}...`))
