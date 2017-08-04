8bf11c04f4d6d023edf88559f133aeb28f9f0b7c
Leonardo Souza <leonardodisouza@gmail.com>
2017-08-03 22:39:09 -0300

    habilitando https

--- libs/boot.js
+++ libs/boot.js
@@ -1,8 +1,19 @@
+import https from 'https'
+import fs from 'fs'
+
 module.exports = (app) => {
   if (process.env.NODE_ENV !== 'test') {
+    const credentials = {
+      key: fs.readFileSync('www.ntask.com.br.key', 'utf8'),
+      cert: fs.readFileSync('www.ntask.com.br.cert', 'utf8')
+    }
+
+    const PORT = app.get('port')
+
     app.db.sequelize.sync().done(() => {
-      const PORT = app.get('port')
-      app.listen(PORT, console.log(`NTask API - Running on port ${PORT}...`))
+      https
+        .createServer(credentials, app)
+        .listen(PORT, console.log(`NTask API - Running on port ${PORT}...`))
     })
   }
 }
